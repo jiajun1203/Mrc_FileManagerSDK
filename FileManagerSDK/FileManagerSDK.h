fc80d7e6d4fc314c0003f37ba99e3d00d5ace237
Mr.陈 <jiajun12_03@163.com>
2020-11-25 21:09:25 +0800

    Initial Commit

---
+++ FileManagerSDK/FileManagerSDK.h
@@ -0,0 +1,18 @@
+//
+//  FileManagerSDK.h
+//  FileManagerSDK
+//
+//  Created by 陈征征 on 2020/11/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FileManagerSDK.
+FOUNDATION_EXPORT double FileManagerSDKVersionNumber;
+
+//! Project version string for FileManagerSDK.
+FOUNDATION_EXPORT const unsigned char FileManagerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileManagerSDK/PublicHeader.h>
+
+
